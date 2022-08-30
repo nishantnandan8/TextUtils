2fd4b9db11d7f0d20584bd351b8f12658687bd93
nishantnandan8 <nishantnandan8@gmail.com>
2022-08-30 18:02:01 +0530

    Updates

---
+++ precache-manifest.cc40eb96ce186ca2fc5239361546e853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62a42ab880fcf6d72f86",
+    "url": "/TextUtils/static/css/main.12b08480.chunk.css"
+  },
+  {
+    "revision": "62a42ab880fcf6d72f86",
+    "url": "/TextUtils/static/js/main.62a42ab8.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "088ecaf4e70570a7b7beac5d1d2f865b",
+    "url": "/TextUtils/index.html"
+  }
+];
